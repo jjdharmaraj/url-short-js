a3c2121dd7652d4f8c90dbfb594b23014837e534
GIDEON <9434592+jjdharmaraj@users.noreply.github.com>
2021-01-18 11:04:25 -0600

    added basic redirect and some static files for the index to use

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require("firebase-functions");
 const express = require("express");
+const { json } = require("express");
 const app = express();
 const cors = require("cors")({ origin: true });
 app.use(cors);
@@ -21,10 +22,28 @@ app.get("/", (req, res) => {
       </body>
     </html>`);
 });
-app.get("/api", (req, res) => {
-  const date = new Date();
-  const hours = (date.getHours() % 12) + 1; // London is UTC + 1hr;
-  res.json({ bongs: "BONG ".repeat(hours) });
+
+app.get("/redirect", (req, res) => {
+  let redirectUrl = "https://www.google.com";
+  res.send(`
+    <!doctype html>
+    <head>
+      <title>goshare.me</title>
+    </head>
+    <body onload="redirect()">
+    <noscript>I am sorry, but you do not have JavaScript enabled.  Click <a href="${redirectUrl}">here</a> to visit your destination.</noscript>
+      <script>
+        function redirect() {
+        location.replace("${redirectUrl}")
+        }
+      </script>
+    </body>
+  </html>`);
+});
+
+//Capture All 404 errors
+app.use(function (req, res, next) {
+  res.status(404).send(`Unable to find the requested resource!`);
 });
 
 exports.app = functions.https.onRequest(app);
